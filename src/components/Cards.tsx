476fdceccef50749904ea2424db42a59664bd27a
Arnaud BEAUGEON <arnaud.beaugeon@live.fr>
2022-03-11 09:45:38 +0100

    [~] Cards structure

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,1 +1,29 @@
-import React from 'react';
+import React from "react";
+import { View, Image, Text } from "react-native";
+
+const Cards = () => {
+  return (
+    <View>
+      <Image source={}>
+      <View>
+        <Text>Britney Spears</Text>
+        <View>
+          <View>
+            <Text>Gender</Text>
+            <Text>Female</Text>
+          </View>
+        </View>
+        <View>
+            <Text>Nationality: </Text>
+            <Text>US</Text>
+        </View>
+        <View>
+            <Text>Occupation</Text>
+            <Text>actor...</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Cards;
