855408a990897aab7fa3c681f01d6252a365339f
Arnaud BEAUGEON <arnaud.beaugeon@live.fr>
2022-03-11 11:16:16 +0100

    [~] Add Infos interface + Add Cards to Home

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import Cards from "./components/Cards";
+
 function App() {
   return (
     <div className="App">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Cards />
     </div>
   );
 }
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,25 +1,55 @@
 import React from "react";
 import { View, Image, Text } from "react-native";
 
-const Cards = () => {
+interface Infos {
+    image: string,
+    name: string,
+    gender: string,
+    age: number,
+    birthday: string,
+    height: number,
+    nationality: string,
+    net_worth: number,
+    occupation: string[]
+};
+
+const Cards = (props:Infos) => {
   return (
     <View>
-      <Image source={}>
+      <Image source={{uri: props.image}} />
       <View>
-        <Text>Britney Spears</Text>
+        <Text>{props.name}</Text>
         <View>
           <View>
             <Text>Gender</Text>
-            <Text>Female</Text>
+            <Text>{props.gender}</Text>
+          </View>
+          <View>
+            <Text>Age</Text>
+            <Text>{props.age}</Text>
+          </View>
+          <View>
+            <Text>Birthday</Text>
+            <Text>{props.birthday}</Text>
+          </View>
+          <View>
+            <Text>Height</Text>
+            <Text>{props.height}</Text>
           </View>
         </View>
         <View>
+        <View>
             <Text>Nationality: </Text>
-            <Text>US</Text>
+            <Text>{props.nationality}</Text>
+        </View>
+        <View>
+            <Text>Net worth: </Text>
+            <Text>{props.net_worth}</Text>
+        </View>
         </View>
         <View>
             <Text>Occupation</Text>
-            <Text>actor...</Text>
+            <Text>{props.occupation}</Text>
         </View>
       </View>
     </View>
